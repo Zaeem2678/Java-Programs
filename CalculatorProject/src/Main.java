ccdd52d8d179a98b6ee69f21a31cc0d7d3a590fb
Zaeem2678 <102031010+Zaeem2678@users.noreply.github.com>
2024-03-21 00:02:27 +0500

    Added Module Addition Module

--- CalculatorProject/src/Main.java
+++ CalculatorProject/src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is my calculator program!");
+        System.out.println("add method");
         }
     }
 }
